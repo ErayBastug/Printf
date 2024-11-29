dd50ad99112b03c63136c824c464b6f533569e45
ErayBastug <142332697+ErayBastug@users.noreply.github.com>
2024-11-29 21:30:32 +0300

    Create ft_putthink.c

---
+++ ft_putthink.c
@@ -0,0 +1,50 @@
+int ft_putnbr(int n)
+{
+    char a;
+    int b;
+
+    b = 0;
+    if (n == -2147483648)
+        b += (write(1, &"-2147483648", 11));
+    else if (n < 0)
+    {
+        b += (write(1, "-", 1));
+        n = n * -1;
+        b += ft_putnbr(n);
+    }
+    else if (n > 9)
+    {
+        b += ft_putnbr(n / 10);
+        a = (n % 10) + '0';
+        b += (write(1, &a, 1));
+    }
+    else
+    {
+        a = n + 48;
+        b += (write(1, &a, 1));
+    }
+    return (b);
+}
+
+int ft_putstr(char *s)
+{
+    int i;
+
+    i = 0;
+    if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+    while (i < strlen(s))
+    {
+        write(1, &s[i], 1);
+        i++;
+    }
+	return (i);
+}
+
+int	ft_putchar(int c)
+{
+	return (write(1, &c, 1));
+}
